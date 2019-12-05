25532737afcd87666c23b6433fe8f80343f0132d
顾浩楠 <376909234@qq.com>
2019-12-05 13:29:18 +0800

    基准

---
+++ health_interface/src/main/java/com/itheima/service/PermissionService.java
@@ -0,0 +1,15 @@
+package com.itheima.service;
+
+import java.util.Map;
+
+/**
+ * 权限服务
+ */
+public interface PermissionService {
+
+    /**
+     * 新增权限
+     * @param map
+     */
+    void add(Map map);
+}
---
+++ health_service_provider/src/main/java/com/itheima/service/impl/PermissionServiceImpl.java
@@ -0,0 +1,28 @@
+package com.itheima.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.itheima.dao.PermissionDao;
+import com.itheima.service.PermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+
+/**
+ * 权限服务
+ */
+@Service(interfaceClass = PermissionService.class)
+@Transactional
+public class PermissionServiceImpl implements PermissionService {
+
+    @Autowired
+    private PermissionDao permissionDao;
+
+    /**
+     * 新增权限
+     * @param map
+     */
+    public void add(Map map) {
+
+    }
+}
